8245c26166e40208a678790e37718fd02daa261f
Damian Soto <soto.platero@gmail.com>
2021-09-28 16:43:10 -0400

    remove tailwind

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import "../app.postcss";
+	// import "../app.postcss";
     import { navigating } from '$app/stores';
     import {onMount} from 'svelte'
     import { session } from '$app/stores';
