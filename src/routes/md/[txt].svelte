273dbb605ae3eda59becad9ec25bc531a7ad0937
Damian Soto <soto.platero@gmail.com>
2021-09-29 23:20:24 -0400

    add md

---
+++ src/routes/md/[txt].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	import marked from 'marked'
+    export async function load({page, fetch}) {
+
+    	page.params.txt.split('#')
+    	console.log(page.params.txt.split('#'))
+    	// const theme = page.params.options
+    	// const res = await fetch(`/blog.json?url=${page.params.txt}`)
+    	// if (!res.ok)
+    	// const blog = await res.json()
+
+		return {
+			props: {  }
+		}
+
+    }	
+</script>
---
+++ src/routes/md/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	let md = ''
+	$: url = `/md/${encodeURIComponent( md ) }`
+</script>
+
+
+<article id="">
+  
+  <section class="" style="">
+<!--     <span class="background-right-bottom" style="background-image:url('../static/images/iphone-hand.png')"></span> -->
+    <div class="wrap size-40 aligncenter">
+<textarea
+	class=""
+	bind:value={md}
+	rows="5"
+></textarea>
+<a href={url} type="submit" tabindex="2" title="URL Blog" class="button"><strong>Publish ›</strong></a>    </div>
+    <!-- .end .wrap -->
+  </section>
+</article>
